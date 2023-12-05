f15ffddce8ed58bcdf1c54572f186f38f68ae603
Replit user <>
2023-12-05 16:29:16 +0000

    feat(stories): add story cw-carousel

--- packages/kyra-components/src/components/cw-carousel/stories/cw-carousel.stories.tsx
+++ packages/kyra-components/src/components/cw-carousel/stories/cw-carousel.stories.tsx
@@ -1,38 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/html';
 import { cwCarouselArgs } from "../types/cw-carousel.args";
 
-export default {
-  title: "carousel",
-};
+const Component: Meta<cwCarouselArgs> = {
+  title: 'Carousel',
+}
 
-const Template = (args: cwCarouselArgs) => `
-  <cw-carousel
-    width=${args.width}
-    height=${args.height}
-    colors=${args.colors}
-    arrows=${args.arrows}
-    index=${args.index},
-    autoplay=${args.autoplay}
-    time=${args.time}>
-      <figure>
-        <img src="/images/carousel-image-1.png" alt="item 1" />
-      </figure>
-      <figure>
-        <img src="/images/carousel-image-2.png" alt="item 2" />
-      </figure>
-      <figure>
-      <img src="/images/carousel-image-3.png" alt="item 3" />
-      </figure>
-  </cw-carousel>
-`;
+export default Component;
 
-export const Example: { args: cwCarouselArgs } = Template.bind({});
+type Story = StoryObj<cwCarouselArgs>
 
-Example.args = {
-  width: "100vw",
-  height: "450px",
-  autoplay: false,
-  arrows: true,
-  colors: "green",
-  index: false,
-  time: 5000,
-};
+export const Example: Story = {
+  argTypes: {
+    colors:{control:"color"},
+    time: {control: { type: 'number', min:500, max:50000, step: 500 }},
+  },
+  args: {
+    width: "100vw",
+    height: "450px",
+    autoplay: false,
+    arrows: true,
+    colors: "green",
+    index: true,
+    time: 5000,
+  },
+  render: (args) =>{
+    return`
+      <cw-carousel
+        width=${args.width}
+        height=${args.height}
+        colors=${args.colors}
+        arrows=${args.arrows}
+        ${args.index && "index"}
+        autoplay=${args.autoplay}
+        time=${args.time}>
+          <figure>
+            <img src="/images/carousel-image-1.png" alt="item 1" />
+          </figure>
+          <figure>
+            <img src="/images/carousel-image-2.png" alt="item 2" />
+          </figure>
+          <figure>
+          <img src="/images/carousel-image-3.png" alt="item 3" />
+          </figure>
+      </cw-carousel>
+     `
+    },
+}
