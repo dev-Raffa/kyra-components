75c25ec98bae56447ebab55e28979a76ef456a69
Rafael da Conceição <113853780+dev-Raffa@users.noreply.github.com>
2023-12-13 03:52:06 +0000

    feat(components): add cw-card

--- packages/kyra-components-react/lib/components/stencil-generated/index.ts
+++ packages/kyra-components-react/lib/components/stencil-generated/index.ts
@@ -7,6 +7,7 @@ import type { JSX } from 'kyra-components';
 
 
 
+export const CwCard = /*@__PURE__*/createReactComponent<JSX.CwCard, HTMLCwCardElement>('cw-card');
 export const CwCarousel = /*@__PURE__*/createReactComponent<JSX.CwCarousel, HTMLCwCarouselElement>('cw-carousel');
 export const CwDraggable = /*@__PURE__*/createReactComponent<JSX.CwDraggable, HTMLCwDraggableElement>('cw-draggable');
 export const CwDraggableBoard = /*@__PURE__*/createReactComponent<JSX.CwDraggableBoard, HTMLCwDraggableBoardElement>('cw-draggable-board');
